c6665258740180db63f3a9b75df822ca74c2fa08
Saurabh619 <nandwana.saurabh619@gmail.com>
2017-10-07 16:50:10 +0530

    uikit

--- src/app/main/main.controller.spec.js
+++ src/app/main/main.controller.spec.js
@@ -6,15 +6,14 @@
     var $timeout;
 
     beforeEach(module('ngStarters'));
-    beforeEach(inject(function(_$controller_, _$timeout_,) {
+    beforeEach(inject(function(_$controller_) {
 
       vm = _$controller_('MainController');
-      $timeout = _$timeout_;
     }));
 
     it('should define more than 5 awesome things', function() {
       expect(angular.isArray(vm.awesomeThings)).toBeTruthy();
       expect(vm.awesomeThings.length === 5).toBeTruthy();
     });
-  });
 })();
+  });
